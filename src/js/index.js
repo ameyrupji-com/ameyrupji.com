590d51bef2d76dfb09eab0dde2c6c6796b98f66d
Amey Rupji <ameyrupji@gmail.com>
2019-02-28 22:13:13 -0600

    Adding skills container and js to go along with it

--- src/js/index.js
+++ src/js/index.js
@@ -125,4 +125,39 @@ $(document).ready(function() {
       }
     });
   });
+
+  var $skills_blocks = $('.unused')
+  $skills_blocks.each(function(){
+    $(this).hide()
+  });
+
+  $('#skills-btn-all').click(function() {
+    $('#skills-main .btn-group .btn').removeClass('btn-primary').addClass('btn-light')
+    $(this).removeClass('btn-light').addClass('btn-primary')
+
+    $skills_blocks.each(function(){
+      $(this).show()
+    });
+  });
+
+  $('#skills-btn-current').click(function() {
+    $('#skills-main .btn-group .btn').removeClass('btn-primary').addClass('btn-light')
+    $(this).removeClass('btn-light').addClass('btn-primary')
+
+    $skills_blocks.each(function(){
+      $(this).hide()
+    });
+  });
+
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+  initMap()
 });
