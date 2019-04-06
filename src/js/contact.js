de064c33b9d13b496d63feefa1154cd214a09998
Amey Rupji <ameyrupji@gmail.com>
2019-04-06 17:51:53 -0500

    Added From Validations

--- src/js/contact.js
+++ src/js/contact.js
@@ -6,81 +6,110 @@ site.contact = (function ($) {
     View = {
         contatForm: {
             formId: '#contact-me-form',
-            nameInputId: '#full-name-input',
-            telephoneInputId: '#telephone-input',
-            emailInputId: '#email-input',
-            messageInputId: '#message-textarea',
+            nameInputId: '#fullNameInput',
+            telephoneInputId: '#telephoneInput',
+            emailInputId: '#emailInput',
+            messageInputId: '#messageTextarea',
             submitBtnId: '#contact-submit-btn',
+            submitBtnSendingId: '#contact-submit-btn i',
             cancelBtnId: '#cancel-btn'
         },
         contactFromAlertsId: '#contact-me-form-alerts'
     },
     Controller = {
         initilizeContactFromSendBtn: function initilizeContactFromSendBtn() {
-            $(View.contatForm.submitBtnId).click(function contactSubmit(e) {
-                $(this).attr('disabled', 'disabled')
-                e.preventDefault();
-                var name_re = /[A-Za-z]{1}[A-Za-z]/;
-                if (!name_re.test($(View.contatForm.nameInputId).val())) {
-                  alert ("Name can not less than 2 char");
-                  return;
-                }
             
-                var mobile_re = /[0-9]{10}/;
-                if (!mobile_re.test($(View.contatForm.telephoneInputId).val())) {
-                  alert ("Please enter valid phone number");
-                  return;
-                }
-            
-                if ($(View.contatForm.emailInputId).val()=="") {
-                  alert ("Please enter your email id");
-                  return;
-                }
-            
-                var email_re = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-                if (!email_re.test($(View.contatForm.emailInputId).val())) {
-                  alert ("Please enter valid email address");
-                  return;
-                }
-            
-                var name = $(View.contatForm.nameInputId).val();
-                var email = $(View.contatForm.emailInputId).val();
-                var message = $(View.contatForm.messageInputId).val();
-                var telephone = $(View.contatForm.telephoneInputId).val();
-                var data = {
-                   name: name,
-                   telephone: telephone,
-                   email: email,
-                   message: message
-                }
-            
-                var contactFromAlert =  $(View.contactFromAlertsId)
 
-                $.ajax({
-                  type: 'POST',
-                  url : 'https://api.beta.ameyrupji.com/email/',
-                  dataType: 'json',
-                  crossDomain: true,
-                  contentType: 'application/json',
-                  data: JSON.stringify(data),
-                  success: function () {
-                    contactFromAlert
-                        .removeClass('alert-danger')
-                        .addClass('alert-success')
-                        .html('Your message has been successfully sent! I will get back at my earliest convinience.')
-                  },
-                  error: function () {
-                    contactFromAlert
-                        .removeClass('alert-success')
-                        .addClass('alert-danger')
-                        .html('Error sending your message! I have taken a note of this and will try to fix this ASAP.')
-                  },
-                  complete: function() {
-                    $(View.contatForm.formId).trigger("reset")
-                    $(View.contatForm.submitBtnId).removeAttr('disabled')
-                  }
+
+            $.validator.addMethod("fullName", function(value, element) {
+                return this.optional(element) || /[A-Za-z]{2,}\s[A-Za-z]{1,}\s{0}[A-Za-z]{0,}/.test( value );
+            }, "Please enter the valid Full Name");
+            $(View.contatForm.submitBtnId).click(function() {
+                $(View.contatForm.formId).validate({
+                    rules: {
+                        fullNameInput: {
+                            required: true,
+                            fullName: true
+                        },
+                        emailInput: {
+                            required: true,
+                            email: true
+                        },
+                        telephoneInput: {
+                            required: true,
+                            phoneUS: true
+                        },
+                        messageTextarea: {
+                            required: true,
+                            minlength: 100
+                        }
+                    },
+                    highlight: function (element) {
+                        $(element)
+                            .closest('.form-label-group')
+                                .removeClass('success')
+                                .addClass('error');
+                    },
+                    success: function (element) {
+                        element
+                            .text('✓')
+                            .addClass('valid')
+                            .closest('.form-label-group')
+                                .removeClass('error')
+                                .addClass('success');
+                    },
+                    invalidHandler: function name() {
+                        console.log('Form Invalid!')
+                    },
+                    submitHandler: function(form) {
+                        console.log('Form Valid! Submitting form.')
+                        $(View.contatForm.submitBtnSendingId).css('display', 'visible')
+                        var name = $(View.contatForm.nameInputId).val();
+                        var email = $(View.contatForm.emailInputId).val();
+                        var message = $(View.contatForm.messageInputId).val();
+                        var telephone = $(View.contatForm.telephoneInputId).val();
+                        var data = {
+                            name: name,
+                            telephone: telephone,
+                            email: email,
+                            message: message
+                        }
+                    
+                        var contactFromAlert =  $(View.contactFromAlertsId)
+                        var url = $(View.contatForm.formId).attr('target')
+                        console.log('Submitting to:')
+                        console.log(url)
+                        console.log('Data:')
+                        console.log(data)
+                        $.ajax({
+                            type: 'POST',
+                            url : url,
+                            dataType: 'json',
+                            crossDomain: true,
+                            contentType: 'application/json',
+                            data: JSON.stringify(data),
+                            success: function () {
+                            contactFromAlert
+                                .removeClass('alert-danger')
+                                .addClass('alert-success')
+                                .html('Your message has been successfully sent! I will get back at my earliest convinience.')
+                            },
+                            error: function () {
+                            contactFromAlert
+                                .removeClass('alert-success')
+                                .addClass('alert-danger')
+                                .html('Error sending your message! I have taken a note of this and will try to fix this ASAP.')
+                            },
+                            complete: function() {
+                                $(View.contatForm.submitBtnSendingId).css('display', 'none')
+                                $(View.contatForm.formId).trigger("reset")
+                                $(View.contatForm.submitBtnId).removeAttr('disabled')
+                            }
+                        });
+                        return false;
+                    }
                 });
-              }) 
+            })
         },
         init: function init() {
             console.log('In contact:init()')
