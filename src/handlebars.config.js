190aacdb4f03cc0889fbf36396cfb1f3b3e199e6
Amey Rupji <ameyrupji@gmail.com>
2019-04-09 18:43:53 -0500

    Lets try to fix the data not showing up

--- src/handlebars.config.js
+++ src/handlebars.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     layouts: 'view/layouts',
     partials: 'view/partials',
-    data: process.env.NODE_ENV === 'development' ? 'view/data' : 'view/data/prod'
+    data: 'view/data'
 };
