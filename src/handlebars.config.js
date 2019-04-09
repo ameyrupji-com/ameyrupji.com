3660d815e5657a4175f9df7b33a86918a8af9fef
Amey Rupji <ameyrupji@gmail.com>
2019-04-09 18:13:20 -0500

    Reorganizing files

--- src/handlebars.config.js
+++ src/handlebars.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    layouts: 'layouts',
-    partials: 'partials',
-    data: process.env.NODE_ENV === 'development' ? 'data_local' : 'data_production',
+    layouts: 'view/layouts',
+    partials: 'view/partials',
+    data: process.env.NODE_ENV === 'development' ? 'view/data' : 'view/data/prod'
 };
