2b53afb192f11764b275560832fac07ff42ca1dc
Amey Rupji <ameyrupji@gmail.com>
2019-06-14 00:30:39 -0500

    GH-47 fix for version and menu

--- src/js/cover.js
+++ src/js/cover.js
@@ -9,9 +9,18 @@ site.cover = (function ($) {
         navbarClass: '.navbar',
         navbarTogglerBtnClass: '.navbar-toggler',
         navbarBtnClass: '.navbar',
-        contentClass: '#jumbotron-content'
+        contentClass: '#jumbotron-content',
+        releaseNumberSpanId: '#release-number-span',
     },
     Controller = {
+        initilizeVersionForMenu: function initilizeVersionForMenu() {
+            // get the latest release 
+            $.ajax({url: "https://api.github.com/repos/ameyrupji/ameyrupji.com/releases/latest", success: function(result){
+              var release_number = result['tag_name'] 
+              console.log('Found release number: ' + release_number)
+              $(View.releaseNumberSpanId).html(release_number);
+            }});
+        },
         scrollFunction: function scrollFunction() {
             // console.log('In cover:scrollFunction()')
             if ( $(window).scrollTop() > Model.scrollStart  && $(window).scrollTop() < ($(window).height() - 200) ) {
@@ -43,17 +52,18 @@ site.cover = (function ($) {
         },
         initilizeMobileNavbarToggleEvents: function initilizMobileNavbarToggleEvents() {
             console.log('In cover:initilizeMobileNavbarToggleEvents()');
-            // $(View.navbarClass).on('shown.bs.collapse', function() {
-            //     console.log('Navbar Closed');
-            //     $('body').addClass('fixed-position')
-            // })
-            // $(View.navbarClass).on('hidden.bs.collapse', function() {
-            //     console.log('Navbar Opened');
-            //     $('body').removeClass('fixed-position')
-            // })
+            $(View.navbarClass).on('shown.bs.collapse', function() {
+                console.log('Navbar Closed');
+                $('body').addClass('fixed-position')
+            })
+            $(View.navbarClass).on('hidden.bs.collapse', function() {
+                console.log('Navbar Opened');
+                $('body').removeClass('fixed-position')
+            })
         },
         init: function init() {
             console.log('In cover:init()')
+            Controller.initilizeVersionForMenu()
             Controller.initilizeNavbarTogglerBtn()
             Controller.initilizeMobileNavbarToggleEvents()
             Controller.scrollFunction()
