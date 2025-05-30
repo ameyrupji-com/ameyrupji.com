10847abb209a9c4a35dc69920a99785e4a43eaf9
Amey Rupji <ameyrupji@gmail.com>
2025-05-30 17:09:05 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="index.html" element={<App />} />
-          <Route path="feature/GH-115-upgrade-to-react/index.html" element={<App />} />
+          <Route path="/index.html" element={<App />} />
+          <Route path="/feature/GH-115-upgrade-to-react/index.html" element={<App />} />
           <Route path="*" element={<App />} />
           <Route element={<App />} />
         </Routes>
